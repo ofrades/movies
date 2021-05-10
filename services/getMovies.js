53810f2493d833ce0bac9426fb53bd1291bdcbfc
Miguel Bastos <mig.silva@gmail.com>
2021-05-10 01:19:31 +0100

    fix: https cors error

--- services/getMovies.js
+++ services/getMovies.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getMovies = async (query) => {
   try {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${query}`
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${query}`
     );
     return response.data;
   } catch (error) {
